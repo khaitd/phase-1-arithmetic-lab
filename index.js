e009765def7215ccea173bf467539bfe97412647
Dmitriy <khaitdmitriy@gmail.com>
2022-03-16 09:28:47 -0400

    commit tree 6e22aefb022b206deb3b43e2c2e5f8d1bd1ff7fd

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+let num1 = 62;
+let num2 =1;
+
+let multiply = num1 * num2;
+
+let random = Math.floor( (Math.random()*10) + 1 );
+
+let num3 = 11;
+let num4 = 7;
+
+let mod = num3%num4;
+
+let max = Math.max(2,5,19,20,8);
